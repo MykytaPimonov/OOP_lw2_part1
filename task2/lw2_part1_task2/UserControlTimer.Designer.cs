1f7e23805197efd7318bc8c2fa9ea32a01b1d56f
MykytaPimonov <145226647+MykytaPimonov@users.noreply.github.com>
2023-09-17 18:45:39 +0300

    task2

---
+++ task2/lw2_part1_task2/UserControlTimer.Designer.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+
+namespace lw2_part1_task2
+{
+    partial class UserControlTimer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timer = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.timer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000D;
+            this.timer.SynchronizingObject = this;
+            this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // UserControlTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserControlTimer";
+            this.Size = new System.Drawing.Size(150, 76);
+            ((System.ComponentModel.ISupportInitialize)(this.timer)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Timers.Timer timer;
+
+        #endregion
+    }
+}
---
+++ task2/lw2_part1_task2/UserControlTimer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace lw2_part1_task2
+{
+    public partial class UserControlTimer : UserControl
+    {
+        public UserControlTimer()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graphics = e.Graphics;
+            graphics.FillRectangle(Brushes.Aqua, 0, 0, this.Width, this.Height);
+            graphics.DrawString(DateTime.Now.ToLongTimeString(), this.Font, new SolidBrush(this.ForeColor), 0, 0);
+        }
+
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}

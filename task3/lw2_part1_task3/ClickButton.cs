7bc95ca8913d8a663ebcac0421df19604cb288b6
MykytaPimonov <145226647+MykytaPimonov@users.noreply.github.com>
2023-09-17 19:00:07 +0300

    task3

---
+++ task3/lw2_part1_task3/ClickButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lw2_part1_task3
+{
+    public class ClickButton : Button
+    {
+        private int countClicks = 0;
+
+        public int Clicks
+        {
+            get => countClicks;
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            countClicks++;
+            base.OnClick(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            Graphics graphics = pevent.Graphics;
+            SizeF size = graphics.MeasureString(Clicks.ToString(), this.Font, this.Width);
+            graphics.DrawString(
+                Clicks.ToString(), 
+                this.Font, 
+                System.Drawing.SystemBrushes.ControlText,
+                this.Width - size.Width - 3,
+                this.Height - size.Height - 3
+            );
+        }
+    }
+}
